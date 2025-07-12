587b355fccfdfc53dcb30b2d753a3f5a8a9d0e1b
zxjian <zxjian80@hotmail.com>
2025-07-13 02:51:32 +0800

    123

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.yanzhuotech.com"];
+pub const RS_PUB_KEY: &str = "DrsKMUrp0gMgOZ6lBNgs5JE2bzgiVYFWRI6szS4pfuY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
